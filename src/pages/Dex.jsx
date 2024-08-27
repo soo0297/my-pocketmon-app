7b75e25f38856459bdcb4806ac8261fef498d3b5
ssuyeong <107804563+soo0297@users.noreply.github.com>
2024-08-27 12:28:57 +0900

    feat: contextAPI 필요없는 부분 지우기

--- src/pages/Dex.jsx
+++ src/pages/Dex.jsx
@@ -1,10 +1,7 @@
-import { useState } from "react";
 import Dashboard from "../components/Dashboard";
 import PokemonList from "../components/PokemonList";
 import MOCK_DATA from "../mock";
-import { PokemonContext } from "../context/Context";
 import styled from "styled-components";
-import { onAdd, onRemove } from "../redux/modules/pokemon";
 
 const AllDex = styled.div`
   background-color: #fff8e8;
@@ -15,21 +12,10 @@ const AllDex = styled.div`
 `;
 
 function Dex() {
-  const [selectedPokemon, setSelectedPokemon] = useState([]);
-
   return (
     <AllDex>
-      <PokemonContext.Provider
-        value={{
-          selectedPokemon,
-          setSelectedPokemon,
-          onAdd,
-          onRemove,
-        }}
-      >
-        <Dashboard />
-        <PokemonList pokemonList={MOCK_DATA} />
-      </PokemonContext.Provider>
+      <Dashboard />
+      <PokemonList pokemonList={MOCK_DATA} />
     </AllDex>
   );
 }
